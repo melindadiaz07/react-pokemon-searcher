fac5aca9ba794d62fd64289c156f2d6e6c39a7dd
Melindadiaz07 <melindadiaz@me.com>
2020-11-02 14:57:39 -0500

    Set up click event for flipping cards

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,21 +1,41 @@
 import React from 'react'
 import { Card } from 'semantic-ui-react'
 
+
+
 class PokemonCard extends React.Component {
+
+  
+  state = {
+    pokeImage: this.props.pokeData.sprites.front
+  }
+
+  flipCard = () => {
+    (this.state.pokeImage === this.props.pokeData.sprites.front) ?
+    this.setState({
+      pokeImage: this.props.pokeData.sprites.back
+    }) :
+    this.setState({
+      pokeImage: this.props.pokeData.sprites.front
+    })
+  }
+
+  
   render() {
+    const {id, name, hp, sprites} = this.props.pokeData
     return (
-      <Card>
+      <Card onClick={() => this.flipCard()} >
         <div>
           <div className="image">
-            <img alt="oh no!" />
+            <img src={ this.state.pokeImage } alt="oh no!" />
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+    <div className="header">{ this.props.pokeData.name }</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              { hp }
             </span>
           </div>
         </div>
--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -5,6 +5,22 @@ import Search from './Search'
 import { Container } from 'semantic-ui-react'
 
 class PokemonPage extends React.Component {
+
+  state = {
+    pokemons: []
+  }
+
+  componentDidMount(){
+    fetch("http://localhost:3000/pokemon")
+    .then(res => res.json())
+    .then(pokeData => {
+      this.setState({
+        pokemons: pokeData
+      })
+      
+      })
+  }
+
   render() {
     return (
       <Container>
@@ -14,7 +30,7 @@ class PokemonPage extends React.Component {
         <br />
         <Search />
         <br />
-        <PokemonCollection />
+        <PokemonCollection pokemons={this.state.pokemons}/>
       </Container>
     )
   }
